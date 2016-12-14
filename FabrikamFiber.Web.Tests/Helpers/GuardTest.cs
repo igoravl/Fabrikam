6557fd50963abe045ffe466e8d0a2ffb6b48e052
Igor Abade V. Leite <igor.abade@lambda3.com.br>
2016-12-14 16:29:02 -0200

    Import source code from Scrum.org's PSD repository

---
+++ FabrikamFiber.Web.Tests/Helpers/GuardTest.cs
@@ -0,0 +1,52 @@
+﻿namespace FabrikamFiber.Web.Tests.Helpers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using FabrikamFiber.Web.Helpers;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class GuardTest
+    {
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ItShouldThrowExceptionIfArgumentIsNull()
+        {
+            Web.Helpers.Guard.ThrowIfNull(null, "value");
+        }
+
+        [Test]
+        public void ItShouldNotThrowExceptionIfArgumentIsNotNull()
+        {
+            Web.Helpers.Guard.ThrowIfNull("this is not null", "value");
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ItShouldThrowExceptionIfArgumentIsNullOrEmpty()
+        {
+            Web.Helpers.Guard.ThrowIfNullOrEmpty(string.Empty, "value");
+        }
+
+        [Test]
+        public void ItShouldNotThrowExceptionIfArgumentIsNotNullOrEmpty()
+        {
+            Web.Helpers.Guard.ThrowIfNullOrEmpty("not null or empty", "value");
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void ItShouldThrowExceptionIfArgumentIsLesserThanZero()
+        {
+            Web.Helpers.Guard.ThrowIfLesserThanZero(-1, "value");
+        }
+
+        [Test]
+        public void ItShouldNotThrowExceptionIfArgumentIsNotLesserThanZero()
+        {
+            Web.Helpers.Guard.ThrowIfLesserThanZero(1, "value");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Fabrikam 2015
+
+After opening the solution, you may have to select Manage NuGet Packages for Solution and click the Restore button to restore the missing packages.
